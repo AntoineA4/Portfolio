5809c24d17f69764c66439f3b00b37f6a5428a7b
Antoine Gadrat <antoinegadrat@hotmail.fr>
2024-10-28 16:53:43 +0100

    fix: images, plus change language for work descriptions

--- src/components/Modals-card/modal.jsx
+++ src/components/Modals-card/modal.jsx
@@ -4,11 +4,16 @@ import Tags from '../../components/Tags/tags';
 import '../Modals-card/modal.scss'; 
 
 function Modal({ isOpen, title, cover, description, tags, onClose }) {
-    
     if (!isOpen) return null; 
 
+    const handleOverlayClick = (e) => {
+        if (e.target.className === 'modal-overlay') {
+            onClose();
+        }
+    };
+
     return (
-        <div className="modal-overlay">
+        <div className="modal-overlay" onClick={handleOverlayClick}>
             <div className="modal-content">
                 <button className="modal-close" onClick={onClose}>X</button>
                 <img src={cover} alt={title} className="modal-cover" />
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import '../cards/cards.scss';
 import PropTypes from 'prop-types';
 import Modal from '../../components/Modals-card/modal'; 
 
 function Card({ id, title, cover, description, tags }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const { i18n } = useTranslation();
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -14,17 +16,20 @@ function Card({ id, title, cover, description, tags }) {
         setIsModalOpen(false);
     };
 
+    const localCover = require(`../../assets/photos/${id}.png`); 
+    const currentDescription = description[i18n.language] || description['en'];
+
     return (
         <>
             <div className="card" onClick={openModal}>
-                <img src={cover} alt={title} className="card-cover" />
+                <img src={localCover} alt={title} className="card-cover" />
                 <div className="card-title">{title}</div>
             </div>
             <Modal 
                 isOpen={isModalOpen} 
                 title={title} 
-                cover={cover} 
-                description={description} 
+                cover={localCover} 
+                description={currentDescription} 
                 tags={tags} 
                 onClose={closeModal} 
             />
@@ -35,7 +40,7 @@ function Card({ id, title, cover, description, tags }) {
 Card.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    cover: PropTypes.string.isRequired,
+    cover: PropTypes.string.isRequired, 
     description: PropTypes.string.isRequired,
     tags: PropTypes.arrayOf(PropTypes.string).isRequired, 
 };
